384abb380d94132e8d0596e1de788d1916864383
kenwaynebrand <kbrand240@gmail.com>
2023-02-17 21:37:30 -0600

    Update Algorithms.cs

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace DeveloperSample.Algorithms
 {
@@ -7,27 +11,39 @@ namespace DeveloperSample.Algorithms
         public static int GetFactorial(int n)
         {
             int factorial = 1;
-            while (n > 1)
+            if (n < 0)
             {
-                factorial *= n;
-                n--;
+                throw new ArgumentOutOfRangeException(nameof(n), $"Factorial being calculated must be greater then zero.");
+            }
+            else if (n > 0)
+            {
+                try
+                {
+                    checked
+                    {
+                        IEnumerable<int> numList = Enumerable.Range(1, n);
+                        factorial = numList.Aggregate((f, s) => f * s);
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
             }
             return factorial;
         }
 
         public static string FormatSeparators(params string[] items)
         {
-            int l = items.Length;
-            int c = 0;
-            string result = "";
-            foreach (var item in items)
+            try
             {
-                c++;
-                if (c == l) { result += " and " + item; } // last Item
-                else if (c == l-1) { result += item; }    // second to last Item
-                else { result += item + ", "; }           // everything else
+                string lastSeperator = items.Count() > 1 ? " and " : "";
+                return string.Join(", ", items.Take(items.Length - 1).ToArray()) + lastSeperator + items[items.Length - 1];
             }
-            return result;
+            catch(Exception e)
+            {
+                throw e;
+            }
         }
     }
 }
