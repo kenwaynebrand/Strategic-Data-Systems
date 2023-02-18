3ea1df9d82a5d7ffe5f88238842712f0367bf0da
kenwaynebrand <kbrand240@gmail.com>
2023-02-17 21:40:00 -0600

    Update ContainerTest.cs

--- Container/ContainerTest.cs
+++ Container/ContainerTest.cs
@@ -5,8 +5,19 @@ namespace DeveloperSample.Container
     internal interface IContainerTestInterface
     {
     }
+    internal interface IContainerTestInterface2
+    {
+    }
 
     internal class ContainerTestClass : IContainerTestInterface
+    {
+    }
+
+    internal class ContainerTestClass2 : IContainerTestInterface
+    {
+    }
+
+    internal class ContainerTestClass3 : IContainerTestInterface2
     {
     }
 
@@ -19,6 +30,27 @@ namespace DeveloperSample.Container
             container.Bind(typeof(IContainerTestInterface), typeof(ContainerTestClass));
             var testInstance = container.Get<IContainerTestInterface>();
             Assert.IsType<ContainerTestClass>(testInstance);
+        }
+
+
+        [Fact]
+        public void CanBindMultipleInterfaceAndGetService()
+        {
+            var container = new Container();
+            container.Bind(typeof(IContainerTestInterface), typeof(ContainerTestClass));
+            container.Bind(typeof(IContainerTestInterface2), typeof(ContainerTestClass3));
+            var testInstance = container.Get<IContainerTestInterface>();
+            Assert.IsType<ContainerTestClass>(testInstance);
+        }
+
+        [Fact]
+        public void CanUpdateBindAndGetService()
+        {
+            var container = new Container();
+            container.Bind(typeof(IContainerTestInterface), typeof(ContainerTestClass));
+            container.Bind(typeof(IContainerTestInterface), typeof(ContainerTestClass2));
+            var testInstance = container.Get<IContainerTestInterface>();
+            Assert.IsType<ContainerTestClass2>(testInstance);
         }
     }
 }
