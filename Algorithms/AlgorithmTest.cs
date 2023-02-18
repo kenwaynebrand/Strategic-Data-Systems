cc898d63d1a2261e832ee88c527b36639406c7a3
kenwaynebrand <kbrand240@gmail.com>
2023-02-17 21:37:58 -0600

    Update AlgorithmTest.cs

--- Algorithms/AlgorithmTest.cs
+++ Algorithms/AlgorithmTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace DeveloperSample.Algorithms
@@ -11,9 +12,52 @@ namespace DeveloperSample.Algorithms
         }
 
         [Fact]
+        public void CanGetFactorialZero()
+        {
+            Assert.Equal(1, Algorithms.GetFactorial(0));
+        }
+
+        [Fact]
+        public void CanGetNegativeFactorial()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Algorithms.GetFactorial(-1));
+        }
+
+        [Fact]
+        public void CanHandleFactorialOverflow()
+        {
+            Assert.Throws<OverflowException>(() => Algorithms.GetFactorial(15));
+        }
+
+        [Fact]
         public void CanFormatSeparators()
         {
             Assert.Equal("a, b and c", Algorithms.FormatSeparators("a", "b", "c"));
+        }
+
+        [Fact]
+        public void CanFormatSeparatorsTwoItems()
+        {
+            Assert.Equal("a and b", Algorithms.FormatSeparators("a", "b"));
+        }
+
+        [Fact]
+        public void CanFormatSeparatorsOneItem()
+        {
+            Assert.Equal("a", Algorithms.FormatSeparators("a"));
+        }
+
+        [Fact]
+        public void CanFormatSeparatorsNoItem()
+        {
+            Assert.Equal("", Algorithms.FormatSeparators(""));
+        }
+
+
+        [Fact]
+        public void CanFormatSeparatorsNullItem()
+        {
+            Assert.Throws<ArgumentNullException>(() => Algorithms.FormatSeparators(null));
         }
     }
 }
