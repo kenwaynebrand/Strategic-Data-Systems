27a9d7d0685e0242d225dc1ed91f15af2fe7d818
kenwaynebrand <kbrand240@gmail.com>
2023-02-18 11:46:36 -0600

    Update ClassRefactor.cs

--- ClassRefactoring/ClassRefactor.cs
+++ ClassRefactoring/ClassRefactor.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using System;
 using System.Reflection;
 
@@ -5,12 +6,12 @@ namespace DeveloperSample.ClassRefactoring
 {
     public interface ISwallowInterface
     {
-        int getUnladenSpeed();
+        int Speed { get; }
     }
 
     public interface ILoadInterface
     {
-        int getLadingPenalty();
+        int Penalty { get; }
     }
 
     public class Swallow
@@ -22,20 +23,27 @@ namespace DeveloperSample.ClassRefactoring
         {
             if (!typeof(ISwallowInterface).IsAssignableFrom(swallowType))
             {
-                throw new ArgumentException(nameof(swallowType), $"Is of the wrong type. This should implement the ISwallowInterface.");
+                throw new ArgumentException(nameof(swallowType), $" Is of the wrong type. This should implement the ISwallowInterface.");
             }
             if (!typeof(ILoadInterface).IsAssignableFrom(swallowLoad))
             {
-                throw new ArgumentException(nameof(swallowLoad), $"Is of the wrong type. This should implement the ILoadInterface.");
+                throw new ArgumentException(nameof(swallowLoad), $" Is of the wrong type. This should implement the ILoadInterface.");
             }
-
             Type = (ISwallowInterface)Activator.CreateInstance(swallowType);
+            if (Type.Speed <= 0)
+            {
+                throw new ArgumentException(nameof(swallowType), $" was created with an incorrect Speed parameter. Speed must by greater than zero.");
+            }
             Load = (ILoadInterface)Activator.CreateInstance(swallowLoad);
+            if (Load.Penalty < 0)
+            {
+                throw new ArgumentException(nameof(swallowLoad), $" was created with an incorrect Penalty parameter. Penalty must by greater than or equal to zero.");
+            }
         }
 
         public double GetAirspeedVelocity()
         {
-            return Type.getUnladenSpeed() - Load.getLadingPenalty();
+            return Type.Speed - Load.Penalty;
         }
     }
 }
