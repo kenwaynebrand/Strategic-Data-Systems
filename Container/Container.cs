e2af1a5eb4ab1fb23b1d6008dc2b70afffc0c085
kenwaynebrand <kbrand240@gmail.com>
2023-02-17 21:39:24 -0600

    Update Container.cs

--- Container/Container.cs
+++ Container/Container.cs
@@ -7,27 +7,29 @@ namespace DeveloperSample.Container
 {
     public class Container
     {
-        public Type _interface;
-        public Type _instance;
-
+        private readonly Dictionary<Type, Type> TypesLookup  = new();
         public void Bind(Type interfaceType, Type implementationType)
         {
-            _interface = interfaceType;
-            _instance = implementationType;
+            if (TypesLookup.ContainsKey(interfaceType)) // verify the interface hasn't been added
+            {
+               TypesLookup.TryGetValue(interfaceType, out Type existingImplementationType);
+                if (implementationType != existingImplementationType) // existing interface, verify it need to be updated
+                {
+                    TypesLookup.Remove(interfaceType);
+                    TypesLookup.Add(interfaceType, implementationType);
+                }
+            }
+            else // new interface, add it to the container
+            {
+                TypesLookup.Add(interfaceType, implementationType);
+            }
         }
 
         public T Get<T>()
         {
-            object res = Activator.CreateInstance(_instance);
-            if (_interface == typeof(T))
-            {
-                return (T)res;
-            }
-            else
-            {
-                return default(T);
-            }
-
+            TypesLookup.TryGetValue(typeof(T), out Type newImplementation);
+            object res = Activator.CreateInstance(newImplementation);
+            return (T)res;
         }
     }
 }
