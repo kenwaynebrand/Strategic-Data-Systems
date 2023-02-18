775663072d6b84115fd746597647bf3eeaa5a527
kenwaynebrand <kbrand240@gmail.com>
2023-02-17 21:38:55 -0600

    Update ClassRefactorTest.cs

--- ClassRefactoring/ClassRefactorTest.cs
+++ ClassRefactoring/ClassRefactorTest.cs
@@ -1,41 +1,71 @@
+using System;
 using Xunit;
 
 namespace DeveloperSample.ClassRefactoring
 {
+
+
+    internal class AfricanSwallow : ISwallowInterface
+    {
+        public int getUnladenSpeed() { return 22; }
+    }
+
+    internal class EuropeanSwallow : ISwallowInterface
+    {
+        public int getUnladenSpeed() { return 20; }
+    }
+
+    internal class CoconutLoad : ILoadInterface
+    {
+        public int getLadingPenalty() { return 4; }
+    }
+
+    internal class NoLoad : ILoadInterface
+    {
+        public int getLadingPenalty() { return 0; }
+    }
+
     public class ClassRefactorTest
     {
+
         [Fact]
         public void AfricanSwallowHasCorrectSpeed()
         {
-            var swallowFactory = new SwallowFactory();
-            var swallow = swallowFactory.GetSwallow(SwallowType.African);
+            var swallow = new Swallow(typeof(AfricanSwallow), typeof(NoLoad));
             Assert.Equal(22, swallow.GetAirspeedVelocity());
         }
 
         [Fact]
         public void LadenAfricanSwallowHasCorrectSpeed()
         {
-            var swallowFactory = new SwallowFactory();
-            var swallow = swallowFactory.GetSwallow(SwallowType.African);
-            swallow.ApplyLoad(SwallowLoad.Coconut);
+            var swallow = new Swallow(typeof(AfricanSwallow), typeof(CoconutLoad));
             Assert.Equal(18, swallow.GetAirspeedVelocity());
         }
 
         [Fact]
         public void EuropeanSwallowHasCorrectSpeed()
         {
-            var swallowFactory = new SwallowFactory();
-            var swallow = swallowFactory.GetSwallow(SwallowType.European);
+            var swallow = new Swallow(typeof(EuropeanSwallow), typeof(NoLoad));
             Assert.Equal(20, swallow.GetAirspeedVelocity());
         }
 
         [Fact]
         public void LadenEuropeanSwallowHasCorrectSpeed()
         {
-            var swallowFactory = new SwallowFactory();
-            var swallow = swallowFactory.GetSwallow(SwallowType.European);
-            swallow.ApplyLoad(SwallowLoad.Coconut);
+            var swallow = new Swallow(typeof(EuropeanSwallow), typeof(CoconutLoad));
             Assert.Equal(16, swallow.GetAirspeedVelocity());
+        }
+
+        [Fact]
+        public void WrongSwallowType()
+        {
+            Assert.Throws<ArgumentException>(() => new Swallow(typeof(CoconutLoad), typeof(CoconutLoad)));
+        }
+
+        [Fact]
+        public void WrongLoadType()
+        {
+            Assert.Throws<ArgumentException>(() =>  new Swallow(typeof(EuropeanSwallow), typeof(EuropeanSwallow)));
         }
     }
 }
